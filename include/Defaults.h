4fcf4097a76c22731fd4ee097b52676a774a603c
Wiktor <wiktor.markowski81@gmail.com>
2026-09-08 12:42:37 +0200

    Create default config file and default main.cpp file

---
+++ include/Defaults.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace defaults
+{
+	extern const std::string_view kDefaultConfigToml;
+	extern const std::string_view kDefaultMainCpp;
+}
---
+++ src/Defaults.cpp
@@ -0,0 +1,35 @@
+#include "Defaults.h"
+#include <string_view>
+
+namespace defaults
+{
+	const std::string_view kDefaultConfigToml = R"toml(
+[project]
+name = "main"
+version = "0.1.0"
+
+[build]
+cpp-version = "23"
+source-directory = "src"
+header-directory = "include"
+
+[tests]
+enable-tests = false
+tests-directory = "tests"
+
+[examples]
+enable-examples = false
+example-directory = "examples"
+	)toml";
+
+	const std::string_view kDefaultMainCpp = R"cpp(
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
+	)cpp";
+}
