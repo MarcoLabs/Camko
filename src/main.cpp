682ba2b3f840a6664356ad8f5b60595fa1becd8a
Wiktor <wiktor.markowski81@gmail.com>
2026-09-08 13:54:19 +0200

    Move definition of TrimArgvFromFirstTwoElements to below main

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,7 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> TrimArgvFromFirstTwoElements(int argc, const char** argv) // first element is always camko and second is the option name
-{
-	std::vector<std::string> trimmedArgv(argv + 2, argv + argc);
-
-	return trimmedArgv;
-}
+std::vector<std::string> TrimArgvFromFirstTwoElements(int argc, const char** argv); // first element is always camko and second is the option name
 
 int main(int argc, const char** argv)
 {
@@ -30,4 +25,11 @@ int main(int argc, const char** argv)
 	}
 
 	return 0;
+}
+
+std::vector<std::string> TrimArgvFromFirstTwoElements(int argc, const char** argv)
+{
+	std::vector<std::string> trimmedArgv(argv + 2, argv + argc);
+
+	return trimmedArgv;
 }
