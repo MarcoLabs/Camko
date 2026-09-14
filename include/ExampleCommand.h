cacad3c5e290c814b5190f377ca99d8db9025c03
Wiktor <wiktor.markowski81@gmail.com>
2026-09-14 13:59:18 +0200

    Declare the ExampleCommand class and implement the ExampleCommand::Name method

---
+++ include/ExampleCommand.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Command.h"
+#include <string>
+#include <vector>
+
+class ExampleCommand : public Command
+{
+public:
+	CommandError Execute(const std::vector<std::string>& args) override;
+	std::string Name() const override;
+};
---
+++ src/ExampleCommand.cpp
@@ -0,0 +1,11 @@
+#include "ExampleCommand.h"
+
+CommandError ExampleCommand::Execute(const std::vector<std::string>& args)
+{
+	
+}
+
+std::string ExampleCommand::Name() const
+{
+	return "example";
+}
